5add77e4a1dc2721ecde76c557610e36f1b0cd48
Ace <acekisch@gmail.com>
2025-10-24 20:00:20 -0700

    add series page header

--- apps/web/src/app/series/page.tsx
+++ apps/web/src/app/series/page.tsx
@@ -1,7 +1,22 @@
+import PageHeader from "@/components/modules/PageHeader";
+
 export default function SeriesPage() {
   return (
-    <section className="container">
-      <h1>Series</h1>
-    </section>
+    <main>
+      <PageHeader
+        title={`Series`}
+        subtitle="Follow the sagas of heroes, traitors, and legends across campaigns, Crusades, and Black Library narrative arcs."
+        // subtitle="Explore narrative arcs, character sagas, and storylines across the Warhammer universe."
+        align="center"
+        strongOverlay
+        height="xs"
+        priority
+        image="/images/eisenhorn-alexander-ovchinnikov.jpg"
+        alt="Eisenhorn by Alexander Ovchinnikov"
+      />
+      <section className="container">
+        <h1>Series</h1>
+      </section>
+    </main>
   );
 }
